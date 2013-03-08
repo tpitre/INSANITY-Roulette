5b58bd2923cdef8aec2a5739d6cd05ee9fac5017
Terrance Pitre <tpitre@gmail.com>
2013-03-07 19:06:17 -0500

    Added a total time counter with custom time selecting

--- js/script.js
+++ js/script.js
@@ -17,12 +17,12 @@ $(document).ready(function() {
 
 	  var countDown = intervalTime / 1000,
 	  		countDownDisplay = intervalTime / 1000,
-				counter = setInterval(timer, 1000); //1000 will run it every 1 second
+				counter = setInterval(intervalTimer, 1000); //1000 will run it every 1 second
 
 		if (i === 0) { $countDown.html(":" + countDown); }
 
-		// add the timer
-		function timer() {
+		// add the interval timer
+		function intervalTimer() {
 		  countDown=countDown-1;
 		  if (countDown <= 0) {
 		     clearInterval(counter);
@@ -32,9 +32,34 @@ $(document).ready(function() {
 		  }
 		  $countDown.html(":" + countDown);
 		}
+
 		i++;
 	};
 
+	// set total time
+	function totalTime() {
+    var $tt = $('.total-time-countdown'),
+    		$m = $('.time-min'),
+    		$s = $('.time-sec');
+
+    if($m.html() == 0 && $s.html() == '00') {
+      $tt.html('Workout Complete!').addClass('wide');
+      clearInterval(totalTime);
+      clearInterval(insanityMoves);
+    }
+    if(parseInt($s.html()) <= 0) {
+      $m.html(parseInt($m.html()-1));
+      $s.html(60);
+    }
+    if(parseInt($s.html()) <= 10) {
+    	$s.html('0' + parseInt($s.html()-1));
+    }
+    else {
+	    $s.html(parseInt($s.html()-1));
+    }
+  }
+
+	// start everything
 	var beginMoves = function(){
 
 		setTimeout(function(){
@@ -43,6 +68,7 @@ $(document).ready(function() {
 
 		setTimeout(function(){
 			insanityMoves();
+			setInterval(totalTime,1000);
 			setInterval(insanityMoves, intervalTime);
 		}, readyTime);
 	};
@@ -51,7 +77,7 @@ $(document).ready(function() {
 		beginMoves();
 		$('.start').addClass('hide');
 		$('.settings').addClass('off')
-		$('.data').removeClass('hide');
+		$('.data, .total-time-countdown').removeClass('hide');
 
 		$("input[type=checkbox]:checked").each(function() {
 			moves.push($(this).val());
@@ -64,14 +90,18 @@ $(document).ready(function() {
 	});
 
 	// change slider value
-	var timeIntervalValue = $('.time-interval-value'),
-			breakIntervalValue = $('.break-interval-value');
+	var $timeIntervalValue = $('.time-interval-value'),
+			$breakIntervalValue = $('.break-interval-value'),
+			$totalMinValue = $('.total-time-value, .time-min');
 
 	$('.time-interval').change(function(){
-	  timeIntervalValue.html(this.value);
+	  $timeIntervalValue.html(this.value);
 	});
 	$('.break-interval').change(function(){
-	  breakIntervalValue.html(this.value);
+	  $breakIntervalValue.html(this.value);
+	});
+	$('.total-time').change(function(){
+	  $totalMinValue.html(this.value);
 	});
 
 	$('.time-interval, .break-interval').change();
